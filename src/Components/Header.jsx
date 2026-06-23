161568618a905d94beed05b4cb27dd97c5738ab7
gourav <gouravsharma20a@gmail.com>
2026-06-23 08:51:20 +0530

    feature: enhanced the logic of search

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
 
-const Header = () => {
+const Header = ({onSearchResult,onClear}) => {
+    const [searchtitle,setSearchTitle] = useState("")
+
+    const handleSearch = async()=>{
+        const response = await fetch(`https://bi-assignment1-xi.vercel.app/events/title/${searchtitle}`)
+        const data = await response.json()
+        if(data.FoundEvent) {
+            onSearchResult(data.FoundEvent)
+        } else {
+            alert("No data found")
+        }
+    }
+
+    const handleClear = () => {
+        setSearchTitle("")
+        onClear()
+    }
+
+
     return (
         <nav className="navbar bg-light border-bottom px-4 py-2">
             <a className="navbar-brand fw-bold" href="/"
@@ -21,8 +40,12 @@ const Header = () => {
                         type="text"
                         className="form-control border-start-0"
                         placeholder="Search by title and t..."
+                        value={searchtitle}
+                        onChange={(e)=>setSearchTitle(e.target.value)}
                         style={{outline: 'none', boxShadow: 'none'}}
                     />
+                    {searchtitle && (<button className='btn text-white' style={{ backgroundColor: '#e8523a' }} onClick={handleSearch}>Submit</button>)}
+                    {searchtitle && (<button className='btn btn-outline-secondary' onClick={handleClear}>Clear</button>)}
                 </div>
             </div>
         </nav>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,71 +1,74 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom' 
-import Header from '../Components/Header.jsx'
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import { useEffect, useState } from 'react'
+  import { useNavigate } from 'react-router-dom' 
+  import Header from '../Components/Header.jsx'
 
-const HomePage = () => {
-    const [events,setEvents] = useState([])
-    const navigate = useNavigate()
-    const [selectedType,setSelectedType] = useState("")
+  const HomePage = () => {
+      const [events,setEvents] = useState([])
+      const [searchedEvent, setSearchedEvent] = useState(null)
+      const navigate = useNavigate()
+      const [selectedType,setSelectedType] = useState("")
 
-    useEffect(()=>{
-        const fetchEvents = async () => {
-          let url = "https://bi-assignment1-xi.vercel.app/events"
-          if (selectedType) {
-            url = `https://bi-assignment1-xi.vercel.app/events/eventType/${selectedType}`
+      useEffect(()=>{
+          const fetchEvents = async () => {
+            let url = "https://bi-assignment1-xi.vercel.app/events"
+            if (selectedType) {
+              url = `https://bi-assignment1-xi.vercel.app/events/eventType/${selectedType}`
+            }
+            const response = await fetch(url)
+            const data = await response.json()
+            setEvents(selectedType ? data.foundevent : data.EventsList)
           }
-          const response = await fetch(url)
-          const data = await response.json()
-          setEvents(selectedType ? data.foundevent : data.EventsList)
-        }
-        fetchEvents()
-    },[selectedType])
+          fetchEvents()
+      },[selectedType])
 
-    // const filteredEvents = selectedType ? events.filter((event)=>(event.typeOfEvent === selectedType)):events
+      const displayEvents = searchedEvent ? [searchedEvent]:events
 
-    return (
-      <>
-      <Header/>
-      <div className="bg-light text-dark">
-          <div className="d-flex justify-content-between align-items-center mb-4">
+      return (
+        <>
+        <Header onSearchResult={(event)=>setSearchedEvent(event)}
+        onClear={()=>setSearchedEvent(null)}
+        />
+        <div className="bg-light text-dark">
+            <div className="d-flex justify-content-between align-items-center mb-4">
 
-            <h1 className="fw-bold">Meetup Events</h1>
-                <select
-                    className="form-select w-auto"
-                    value={selectedType}
-                    onChange={(e) => setSelectedType(e.target.value)}
-                >
-                    <option value="">Select Event Type</option>
-                      <option value="offline">Offline Event</option>
-                      <option value="online">Online Event</option>
-                </select>
-          </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          {events.map((event)=>(
-            <div className="col" key={event._id} onClick={()=>navigate(`/events/${event._id}`)} style={{cursor:'pointer'}}>
-    <div className="card h-100">
+              <h1 className="fw-bold">Meetup Events</h1>
+                  <select
+                      className="form-select w-auto"
+                      value={selectedType}
+                      onChange={(e) => setSelectedType(e.target.value)}
+                  >
+                      <option value="">Select Event Type</option>
+                        <option value="offline">Offline Event</option>
+                        <option value="online">Online Event</option>
+                  </select>
+            </div>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {displayEvents.map((event)=>(
+              <div className="col" key={event._id} onClick={()=>navigate(`/events/${event._id}`)} style={{cursor:'pointer'}}>
+      <div className="card h-100">
 
-      <div className="position-relative">
-                                <img src={event.eventImage} className="card-img-top" alt={event.eventTitle}/>
-                                <span className="position-absolute top-0 start-0 m-2 badge bg-white text-dark shadow-sm">
-                                    {event.typeOfEvent}
-                                </span>
-                            </div>
-      <div className="card-body">
-        <p className="card-text">{event.createdAt}</p>
-        <h5 className="card-title">{event.eventTitle}</h5>
+        <div className="position-relative">
+                                  <img src={event.eventImage} className="card-img-top" alt={event.eventTitle}/>
+                                  <span className="position-absolute top-0 start-0 m-2 badge bg-white text-dark shadow-sm">
+                                      {event.typeOfEvent}
+                                  </span>
+                              </div>
+        <div className="card-body">
+          <p className="card-text">{event.createdAt}</p>
+          <h5 className="card-title">{event.eventTitle}</h5>
+        </div>
       </div>
+      </div>
+            )
+        )
+      }
     </div>
     </div>
-          )
+
+        </>
+          
       )
-    }
-  </div>
-  </div>
+  }
 
-      </>
-        
-    )
-}
-
-export default HomePage
+  export default HomePage
